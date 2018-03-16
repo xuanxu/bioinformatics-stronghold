6276e102f15ffd489582ab2a89d4c0d440070140
Juanjo Bazán <jjbazan@gmail.com>
2018-03-16 12:33:11 +0100

    add subs

---
+++ 09-subs/subs.py
@@ -0,0 +1,22 @@
+# Finding a Motif in DNA
+
+# Sample Dataset
+# GATATATGCATATACTT
+# ATAT
+
+# Sample Output
+# 2 4 10
+
+def find_motif(dna, motif):
+  indexes = []
+  start_at = 0
+  while True:
+    i = dna.find(motif, start_at)
+    if i == -1:
+      break
+
+    indexes.append(i)
+    start_at = i + 1
+
+  # output data shoul be space separated 1-based numbering indexes:
+  return ' '.join(str(x + 1) for x in indexes)
