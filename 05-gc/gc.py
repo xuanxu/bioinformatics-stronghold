818c6c4e8203ebdf0a344bb7b366d90db82634a3
Juanjo Bazán <jjbazan@gmail.com>
2018-03-16 11:47:47 +0100

    add solution 5 - python

--- 05-gc/gc.py
+++ 05-gc/gc.py
@@ -6,3 +6,26 @@
 # Sample Output
 # Rosalind_0808
 # 60.919540
+
+dna_strings = {}
+highest_gc_score = 0
+highest_gc_score_id = ""
+
+with open('sample_dataset.txt') as fasta:
+  for line in fasta:
+    if len(line) > 0 and line[0] == '>':
+      name = line[1::].strip()
+      dna_strings[name] = ""
+    else:
+      dna_strings[name] += line.strip()
+
+for k in dna_strings.keys():
+  dna_string = dna_strings[k]
+  gc_score = round((dna_string.count('C') + dna_string.count('G')) / len(dna_string) * 100, 6)
+
+  if gc_score > highest_gc_score:
+    highest_gc_score = gc_score
+    highest_gc_score_id = k
+
+print(highest_gc_score_id)
+print(highest_gc_score)
