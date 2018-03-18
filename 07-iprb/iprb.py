04573ea91fcd13c1da794fe3eade8113a2a33259
Juanjo Bazán <jjbazan@gmail.com>
2018-03-18 19:17:06 +0100

    add python solution to iprb

--- 07-iprb/iprb.py
+++ 07-iprb/iprb.py
@@ -7,3 +7,17 @@
 # 0.78333
 
 def dominant_allele_probability(k, m, n):
+  total = k + m + n
+  k1 = k/total
+  m1 = m/total
+  n1 = n/total
+  k2 = k/(total-1)
+  m2 = m/(total-1)
+  n2 = n/(total-1)
+  mn1 = (m + n)/(total)
+  mn2 = (m + n)/(total-1)
+  m1_m2 = (m-1)/(total-1)
+  k1_k2 = (k-1)/(total-1)
+
+  return (k1 * mn2) + (mn1 * k2) + (k1 * k1_k2) + (0.75 * m1 * m1_m2) + (0.5 * m1 * n2) + (0.5 * n1 * m2)
+
