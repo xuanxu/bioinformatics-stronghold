bcede101420c847bdd0ff34e6346fd83d3e3ec72
Juanjo Bazán <jjbazan@gmail.com>
2018-03-20 12:56:35 +0100

    add cons placeholder

---
+++ 010-cons/cons.py
@@ -0,0 +1,12 @@
+# Consensus and Profile
+
+# Sample Dataset
+# sample_dataset.txt
+
+# Sample Output
+# sample_output.txt
+
+input_file = "sample_dataset.txt"
+output_file = "sample_output.txt"
+
+def cons(input_file, output_file):
