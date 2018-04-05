59adefad2b80ce221bf0607e29666499b43dac29
Juanjo Bazán <jjbazan@gmail.com>
2018-04-05 16:55:58 +0200

    delete unnecessary colon

--- 010-cons/cons.py
+++ 010-cons/cons.py
@@ -11,7 +11,7 @@ def fasta_file_to_dna_strings(fasta_file):
   with open(fasta_file) as fasta:
     for line in fasta:
       if len(line) > 0 and line[0] == '>':
-        name = line[1::].strip()
+        name = line[1:].strip()
         dna_strings[name] = ''
       else:
         dna_strings[name] += line.strip()
